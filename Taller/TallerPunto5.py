c0dd7994bb352e5d917e94c4cf4b70a3c4de93c8
cristofer2627 <cristofer2627@gmail.com>
2019-09-05 22:48:06 -0500

    Taller Retos Phyton Robin Ruales, Cristofer Burbano

--- Taller/TallerPunto5.py
+++ Taller/TallerPunto5.py
@@ -1,11 +1,12 @@
+#Created by Cristofer Burbano, Robin Ruales
 import os
+
+n2 = []
 
  
 def menu():
-	"""
-	Función que limpia la pantalla y muestra nuevamente el menu
-	"""
-	os.system('clear') # NOTA para windows tienes que cambiar clear por cls
+	
+	os.system('cls')
 	print ("Selecciona una opción")
 	print ("\t1 - Ingresar Palabra")
 	print ("\t2 - Mostrar  Palabra")
@@ -13,33 +14,52 @@ def menu():
 	print ("\t4 - salir")
 
 def op1():
-    n1 = int(input("¿cuantas letras tiene la palabra que decea ingresar?"))
+    n1 = int(input("¿cuantas letras tiene la palabra que desea ingresar?"))
     i=1
-    n2=[]
+    global n2
+    n2 = []
+    
     
     while i <= n1 :
         print("Digite letra ", i)
         b=input()
         n2.insert(i,b)
         i = i + 1 
+    print(n2)
+    key = input("pulse ENTER para continuar")
            
+def op2():
+    global n2
+    for letra in n2:
+        print(letra)
+    key = input("pulse ENTER para continuar")
+
+def op3():
+    global n2
+    var1 = len(n2)
     
- 
+    while var1 > 0 :
+        print(n2[var1 - 1])
+        var1 = var1 - 1
+    key = input("pulse ENTER para continuar")
+    
+
+  
 while True:
-	# Mostramos el menu
+	#menu
 	menu()
  
-	# solicituamos una opción al usuario
 	opcionMenu = input("inserta un numero valor >> ")
 	if opcionMenu=="1":
 		op1()           
 
 	elif opcionMenu=="2":
-		print ("")
-		input("Has pulsado la opción 2...\npulsa una tecla para continuar")
+		op2()
+        
+		
 	elif opcionMenu=="3":
-		print ("")
-		input("Has pulsado la opción 3...\npulsa una tecla para continuar")
+		op3()
+
 	elif opcionMenu=="4":
 		break
 	else:
