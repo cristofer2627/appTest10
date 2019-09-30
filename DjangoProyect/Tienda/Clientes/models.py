417e722e5137e12565d2eb1777c4416830e170ef
cristofer2627 <cristofer2627@gmail.com>
2019-09-30 10:21:01 -0500

    clase 30/09/2019

--- DjangoProyect/Tienda/Clientes/models.py
+++ DjangoProyect/Tienda/Clientes/models.py
@@ -2,9 +2,16 @@ from django.db import models
 
 class Preguntas(models.Model):
     preguntas_text = models.CharField(max_length=200)
+    description = models.CharField(max_length=300)
     pub_date = models.DateTimeField('date published')
 
 class Opcion(models.Model):
     preguntas = models.ForeignKey(Preguntas, on_delete=models.CASCADE)
     opcion_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+class Users(models.Model):
+    user_name = models.CharField(max_length=500)
+    email = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
--- DjangoProyect/market/Clients/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('', views.hello, name='hello'),
-    path('', views.ListClients, name='list'),
-]
